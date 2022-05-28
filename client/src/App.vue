4e800a5ef0c0954e4e1a5098861ce25fb5f281b3
DarcruX.Gaurav <kpguru20001@gmail.com>
2022-05-28 19:25:00 +0530

    1st commit

---
+++ client/src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import type { GlobalThemeOverrides } from "naive-ui";
+import { NConfigProvider, darkTheme, NGlobalStyle } from "naive-ui";
+
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    primaryColor: "#FFD43A",
+    primaryColorHover: "#FFE26B",
+    primaryColorPressed: "#DBB02A",
+    fontFamily: "Poppins, Karla",
+    borderRadius: "6px",
+  },
+  Card: {
+    color: "#0d0d0d",
+  },
+};
+</script>
+
+<template>
+  <n-config-provider
+    class="h-full font-poppins"
+    :theme-overrides="themeOverrides"
+    :theme="darkTheme"
+  >
+    <RouterView />
+    <n-global-style />
+  </n-config-provider>
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  font-weight: normal;
+  height: 100%;
+}
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { NButton, NIcon } from "naive-ui";
+import { LinkSquare16Regular } from "@vicons/fluent";
+</script>
+<template>
+  <main class="h-full">
+    <div
+      class="h-full w-full flex flex-row flex-nowrap justify-center items-center"
+    >
+      <div
+        class="max-w-sm w-full rounded flex flex-col flex-nowrap justify-center items-center bg-raisinBlack-80 py-6 px-8"
+      >
+        <h1 class="text-h2 font-bold text-cyberYellow-100">DeEasy</h1>
+        <div class="text-subtitle1 font-semibold">
+          A simpler new world order
+        </div>
+        <n-button
+          type="primary"
+          :ghost="false"
+          block
+          strong
+          class="bg-cyberYellow-100 mt-4"
+          ><template #icon>
+            <n-icon>
+              <link-square-16-regular />
+            </n-icon> </template
+          >Connect you wallet</n-button
+        >
+      </div>
+    </div>
+  </main>
+</template>
